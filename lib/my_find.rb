0210a9eeb1b81c6a957b6957d8671b8695730678
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 21:28:27 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,12 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-  yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end 
     i = i + 1
   end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
